c027758017dbe32996fc195f9db028879b07e38b
Jorge Aparicio <japaricious@gmail.com>
2015-02-18 11:20:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+[![Build Status][status]](https://travis-ci.org/japaric/assign.rs)
+
+# `assign.rs`
+
+Assignment operator traits
+
+# [Documentation][docs]
+
+# License
+
+assign.rs is dual licensed under the Apache 2.0 license and the MIT license.
+
+See LICENSE-APACHE and LICENSE-MIT for more details.
+
+[docs]: http://japaric.github.io/assign.rs/assign/
+[status]: https://travis-ci.org/japaric/assign.rs.svg?branch=master
---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+//! Assignment operators (`+=`, `-=`, `*=`, etc) traits
+//!
+//! Note that no operator sugar is provided, i.e. `x += y` won't use these traits.
+//!
+//! This crate is only  a convenience for me, as I've the need of using these traits as bounds in
+//! some of my libraries, and I'd rather not duplicate the definitions in each library.
+//!
+//! This crate will be deprecated once these traits land in stdlib. (See rust-lang/rfcs#393)
+
+#![deny(missing_docs)]
+#![deny(warnings)]
+
+/// The `+=` operator (without the sugar)
+pub trait AddAssign<Rhs=Self> {
+    /// `a += b` -> `AddAssign::add_assign(&mut a, &b)`
+    fn add_assign(&mut self, &Rhs);
+}
+
+/// The `*=` operator (without the sugar)
+pub trait MulAssign<Rhs=Self> {
+    /// `a *= b` -> `Mulssign::mul_assign(&mut a, &b)`
+    fn mul_assign(&mut self, &Rhs);
+}
+/// The `-=` operator (without the sugar)
+pub trait SubAssign<Rhs=Self> {
+    /// `a -= b` -> `SubAssign::sub_assign(&mut a, &b)`
+    fn sub_assign(&mut self, &Rhs);
+}
